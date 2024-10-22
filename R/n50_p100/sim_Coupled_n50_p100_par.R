e19c5cf641496dfc2f2cec0e70548339afc73849
cbusatto <122295393+cbusatto@users.noreply.github.com>
2024-10-22 18:20:21 +0200

    Add files via upload

---
+++ R/n50_p100/sim_Coupled_n50_p100_par.R
@@ -0,0 +1,230 @@
+require(mvtnorm)
+require(pROC)
+require(mGHS)
+require(GHS)
+require(JGL)
+library(StatPerMeCo)
+library(foreach)
+library(doMC)
+registerDoMC(25)
+
+source("utils_simulations.R")
+source('BIC_GemBag.R')
+Rcpp::sourceCpp('GemBag-algo.cpp')
+
+load("c_p100_Coupled.RData")
+
+get_hyp_ta = function(mu, var) {
+  a = mu^2 * (1 - mu) / var - mu
+  b = (a - mu * a) / mu
+  
+  return(c(a, b))
+}
+
+set.seed(10)
+K = 4
+p = 100
+
+N_iter = 50
+
+B = 10000
+bn = 5000
+
+tm = true_model(list(c1, c2, c3, c4))
+
+## (2.17, 1.78)
+hyp_ta = get_hyp_ta(0.6, 0.05)
+
+## ::::::::::::::::::::::::::
+## n = 50
+
+n = rep(50, K)
+
+RES = matrix(0, N_iter, 7*24 + 1)
+
+cpar = foreach (is = 1:N_iter, .combine = rbind, .packages = c("Rcpp", "mvtnorm", "pROC", "mGHS", "GHS", "JGL", "StatPerMeCo")) %dopar% {
+  
+  print(":::::::::: iteration: ")
+  print(is)
+  Y1 = rmvnorm(n[1], rep(0, p), solve(c1))
+  Y2 = rmvnorm(n[2], rep(0, p), solve(c2))
+  Y3 = rmvnorm(n[3], rep(0, p), solve(c3))
+  Y4 = rmvnorm(n[4], rep(0, p), solve(c4))
+  
+  S = list()
+  S[[1]] = t(Y1) %*% Y1
+  S[[2]] = t(Y2) %*% Y2
+  S[[3]] = t(Y3) %*% Y3
+  S[[4]] = t(Y4) %*% Y4
+  
+  S_jgl = list()
+  S_jgl[[1]] = Y1
+  S_jgl[[2]] = Y2
+  S_jgl[[3]] = Y3
+  S_jgl[[4]] = Y4
+  
+  S_GBAG = list()
+  S_GBAG[[1]] = cov(Y1)
+  S_GBAG[[2]] = cov(Y2)
+  S_GBAG[[3]] = cov(Y3)
+  S_GBAG[[4]] = cov(Y4)
+  
+  ## ::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  ## multiple Graphical Horseshoe
+  print("\n")
+  print(":::: mGHS: ")
+  
+  res_mGHS = mGHS(B, bn, n, S, p, hyp_ta, chain = 0, ping = 1000)
+  
+  print("\n")
+  print("stuck iter: ") 
+  print(is)
+  print(": ")
+  print(res_mGHS$stuck)
+  
+  ## ::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  ## Graphical Horseshoe
+  print("\n")
+  print(":::: GHS: ")
+  
+  res_GHS1 = GHS_est(S[[1]], n[1], bn, B-bn)
+  res_GHS2 = GHS_est(S[[2]], n[2], bn, B-bn)
+  res_GHS3 = GHS_est(S[[3]], n[3], bn, B-bn)
+  res_GHS4 = GHS_est(S[[4]], n[4], bn, B-bn)
+  
+  ## ::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  ## Joint Graphical Lasso
+  print("\n")
+  print(":::: JGL: ")
+  
+  #  Lambda1 options (tuning parameter for graphical lasso penalty)
+  lambda1_opts <- seq(0.1, 0.5, by = 0.02)
+  
+  # Lambda2 options (tuning parameter for fused or group lasso penalty)
+  lambda2_opts_fgl <- c(0.0004, seq(0.0008, 0.02, by = 0.0008))
+  lambda2_opts_ggl <- c(0, 0.0001, 0.0002, 0.0004, seq(0.0008, 0.016, by = 0.0008))
+  
+  # Run fused joint graphical lasso
+  lambda = get_optimal_params(S_jgl, "fused", lambda1_opts, lambda2_opts_fgl)
+  res_fJGL = JGL(Y = S_jgl, penalty = "fused", lambda1 = lambda[1], lambda2 = lambda[2],
+                 return.whole.theta = TRUE)
+  
+  # Run group graphical lasso
+  lambda = get_optimal_params(S_jgl, "group", lambda1_opts, lambda2_opts_ggl)
+  res_gJGL = JGL(Y = S_jgl, penalty = "group", lambda1 = lambda[1], lambda2 = lambda[2],
+                 return.whole.theta = TRUE)
+  
+  # ------------------------ Joint Estimation of Multiple Graphical Models ------------------------ 
+  
+  print("\n")
+  print(":::: GBAG: ")
+  
+  # Set of hyperparameter
+  v0_l = c(0.25, 0.5, 0.75, 1) * sqrt(1 / n / log(p))
+  v1_l = c(2.5, 5, 7.5, 10) * sqrt(1 / n / log(p))
+  p1 = 0.4
+  
+  # Tuning by BIC
+  hyper = Tune_GemBag(v0_l, v1_l, S_GBAG, n, maxiter = 20, p1, p_2 = 0.8)
+  v0 = hyper$v0
+  v1 = hyper$v1
+  
+  # Output:
+  #   Theta: estimated precision matrices
+  #   P: estimated posterior inclusion probabilities
+  #   W: estimated covariance matrices
+  res_GBAG = GemBag(S_l = S_GBAG, n = n, v_0 = v0, v_1 = v1, tau = v0, p_1 = p1, p_2 = 0.8, maxiter = 20)
+  names(res_GBAG) = c('Theta', 'P', 'W')
+  
+  ## ::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  ## Performance indexes
+  
+  ## mGHS
+  
+  tmp_mGHS = tmp_mGHS2 = matrix(0, 4, 7)
+  if (res_mGHS$stuck == 0) {
+    tmp_mGHS = summary_mGHS(res_mGHS, tm, list(c1, c2, c3, c4))
+    
+    # pi_chain = res_mGHS$pi_chain
+    # ta_est = get_ta(2500, K, pi_chain, 30, 25)
+    # ta_m = apply(ta_est$ta[-c(1:1000), ], 2, mean)
+    
+    tmp_mGHS2 = summary_mGHS2(res_mGHS, tm, list(c1, c2, c3, c4), res_mGHS$ta)
+  }
+  
+  ## GHS
+  tmp_GHS_g1 = summary_GHS(res_GHS1, tm[, 1], c1)
+  tmp_GHS_g2 = summary_GHS(res_GHS2, tm[, 2], c2)
+  tmp_GHS_g3 = summary_GHS(res_GHS3, tm[, 3], c3)
+  tmp_GHS_g4 = summary_GHS(res_GHS4, tm[, 4], c4)
+  
+  ## fJGL
+  tmp_fJGL = summary_JGL(res_fJGL, tm, list(c1, c2, c3, c4))
+  
+  ## gJGL
+  tmp_gJGL = summary_JGL(res_gJGL, tm, list(c1, c2, c3, c4))
+  
+  ## GBAG
+  tmp_GBAG = summary_GBAG(res_GBAG, tm, list(c1, c2, c3, c4))
+  
+  RES[is, ] = c(tmp_mGHS[1, ], tmp_mGHS[2, ], tmp_mGHS[3, ], tmp_mGHS[4, ],
+                tmp_mGHS2[1, ], tmp_mGHS2[2, ], tmp_mGHS2[3, ], tmp_mGHS2[4, ],
+                tmp_GHS_g1, tmp_GHS_g2, tmp_GHS_g3, tmp_GHS_g4,
+                tmp_fJGL[1, ], tmp_fJGL[2, ], tmp_fJGL[3, ], tmp_fJGL[4, ],
+                tmp_gJGL[1, ], tmp_gJGL[2, ], tmp_gJGL[3, ], tmp_gJGL[4, ],
+                tmp_GBAG[1, ], tmp_GBAG[2, ], tmp_GBAG[3, ], tmp_GBAG[4, ], res_mGHS$stuck)
+}
+
+res_mean_final = apply(cpar, 2, mean)
+res_sd_final = apply(cpar, 2, sd)
+
+res_g1_mean = matrix(c(res_mean_final[c(1:7, 29:35, 57:63, 85:91, 113:119, 141:147)]), 6, 7, byrow = T)
+colnames(res_g1_mean) = c("Acc", "MCC", "TPR", "FPR", "AUC", "Fr Loss", "Fr2 Loss")
+rownames(res_g1_mean) = c("mGHS", "mGHS2", "GHS", "fJGL", "gJGL", "GBAG")
+
+res_g2_mean = matrix(c(res_mean_final[c(8:14, 36:42, 64:70, 92:98, 120:126, 148:154)]), 6, 7, byrow = T)
+colnames(res_g2_mean) = c("Acc", "MCC", "TPR", "FPR", "AUC", "Fr Loss", "Fr2 Loss")
+rownames(res_g2_mean) = c("mGHS", "mGHS2", "GHS", "fJGL", "gJGL", "GBAG")
+
+res_g3_mean = matrix(c(res_mean_final[c(15:21, 43:49, 71:77, 99:105, 127:133, 155:161)]), 6, 7, byrow = T)
+colnames(res_g3_mean) = c("Acc", "MCC", "TPR", "FPR", "AUC", "Fr Loss", "Fr2 Loss")
+rownames(res_g3_mean) = c("mGHS", "mGHS2", "GHS", "fJGL", "gJGL", "GBAG")
+
+res_g4_mean = matrix(c(res_mean_final[c(22:28, 50:56, 78:84, 106:112, 134:140, 162:168)]), 6, 7, byrow = T)
+colnames(res_g4_mean) = c("Acc", "MCC", "TPR", "FPR", "AUC", "Fr Loss", "Fr2 Loss")
+rownames(res_g4_mean) = c("mGHS", "mGHS2", "GHS", "fJGL", "gJGL", "GBAG")
+
+res_g1_sd = matrix(c(res_sd_final[c(1:7, 29:35, 57:63, 85:91, 113:119, 141:147)]), 6, 7, byrow = T)
+colnames(res_g1_sd) = c("Acc", "MCC", "TPR", "FPR", "AUC", "Fr Loss", "Fr2 Loss")
+rownames(res_g1_sd) = c("mGHS", "mGHS2", "GHS", "fJGL", "gJGL", "GBAG")
+
+res_g2_sd = matrix(c(res_sd_final[c(8:14, 36:42, 64:70, 92:98, 120:126, 148:154)]), 6, 7, byrow = T)
+colnames(res_g2_sd) = c("Acc", "MCC", "TPR", "FPR", "AUC", "Fr Loss", "Fr2 Loss")
+rownames(res_g2_sd) = c("mGHS", "mGHS2", "GHS", "fJGL", "gJGL", "GBAG")
+
+res_g3_sd = matrix(c(res_sd_final[c(15:21, 43:49, 71:77, 99:105, 127:133, 155:161)]), 6, 7, byrow = T)
+colnames(res_g3_sd) = c("Acc", "MCC", "TPR", "FPR", "AUC", "Fr Loss", "Fr2 Loss")
+rownames(res_g3_sd) = c("mGHS", "mGHS2", "GHS", "fJGL", "gJGL", "GBAG")
+
+res_g4_sd = matrix(c(res_sd_final[c(22:28, 50:56, 78:84, 106:112, 134:140, 162:168)]), 6, 7, byrow = T)
+colnames(res_g4_sd) = c("Acc", "MCC", "TPR", "FPR", "AUC", "Fr Loss", "Fr2 Loss")
+rownames(res_g4_sd) = c("mGHS", "mGHS2", "GHS", "fJGL", "gJGL", "GBAG")
+
+res_Coupled_g1_mean = res_g1_mean
+res_Coupled_g2_mean = res_g2_mean
+res_Coupled_g3_mean = res_g3_mean
+res_Coupled_g4_mean = res_g4_mean
+
+res_Coupled_g1_sd = res_g1_sd
+res_Coupled_g2_sd = res_g2_sd
+res_Coupled_g3_sd = res_g3_sd
+res_Coupled_g4_sd = res_g4_sd
+
+res_Coupled_mean = res_Coupled_g1_mean + res_Coupled_g2_mean + res_Coupled_g3_mean + res_Coupled_g4_mean
+res_Coupled_sd = res_Coupled_g1_sd + res_Coupled_g2_sd + res_Coupled_g3_sd + res_Coupled_g4_sd
+
+stuck_index = sum(cpar[, 169])
+
+save(stuck_index, res_Coupled_mean, res_Coupled_g1_mean, res_Coupled_g2_mean, res_Coupled_g3_mean, res_Coupled_g4_mean, file = "sim_n50_p100_mean_Coupled.RData")
+save(res_Coupled_sd, res_Coupled_g1_sd, res_Coupled_g2_sd, res_Coupled_g3_sd, res_Coupled_g4_sd, file = "sim_n50_p100_sd_Coupled.RData")
+
